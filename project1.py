5703694d61c75482af64b978a33687202d4026d9
sanjana2013 <166921916+sanjana2013@users.noreply.github.com>
2024-04-13 18:15:36 +0530

    Add files via upload

---
+++ project1.py
@@ -0,0 +1,19 @@
+#Traffic Light Simulator
+
+
+
+
+trafficlight=input("Enter a vegetable(red,yellow,green ): ")
+trafficlight=trafficlight.lower()
+if trafficlight=="red":
+     print("stop")
+elif (trafficlight=="yellow"):
+     print("wait")
+
+elif (trafficlight=="green"):
+    print("go")
+
+else:
+    print("choose from option")
+
+
