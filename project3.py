abeb62ae787ec8957653ae3c071b851de4db29f3
sanjana2013 <166921916+sanjana2013@users.noreply.github.com>
2024-05-12 09:01:40 +0530

    Add files via upload

---
+++ project3.py
@@ -0,0 +1,11 @@
+g=[90,89,40,55,97,45,13,43,47,10,4766,800,700]
+even=[]
+odd=[]
+for i in g:
+    if i%2==0:
+        even.append(i)
+    else:
+        odd.append(i)
+print("Even.list",even)
+print("Odd.list",odd)
+
